a9a38511c4124372d30f989c74700cd098901b29
bashayer_nouri <bashayer_nouri@hotmail.com>
2019-10-09 16:35:15 +0300

    Edit files

--- components/ChannelList.js
+++ components/ChannelList.js
@@ -3,29 +3,46 @@ import * as actionCreators from "../redux/actions";
 import { connect } from "react-redux";
 
 //NativeBase Components
-import { Text, Left, Body, Right, Button, ListItem, Icon } from "native-base";
+import {
+  Text,
+  Left,
+  Body,
+  Right,
+  Button,
+  ListItem,
+  Icon,
+  Header,
+  Title,
+  Container
+} from "native-base";
 import { View } from "react-native";
 
 //Components
 import ChannelRow from "./ChannelRow";
 
 class ChannelList extends Component {
-  componentDidMount() {
-    //main();
-    this.props.fetchChannels();
-  }
+  // componentDidMount() {
+  //   //main();
+  //   this.props.fetchChannels();
+  // }
 
   state = {
     name: "" //channel name
   };
 
   render() {
-    const channelRows = this.props.channels.map(channel => (
-      <ChannelRow key={channel.name} channel={channel} />
-    ));
+    const channelRows = this.props.channels.map(channel => {
+      return <ChannelRow channel={channel} />;
+    });
+
+    // const channelRows = this.props.channels.map(channel => {
+    //   console.log(channel);
+
+    //   return <Text onPress={() => alert("hi")}>{channel.name}</Text>;
+    // });
 
     return (
-      <View>
+      <Container>
         <Header>
           <Title>Channels List</Title>
 
@@ -33,9 +50,8 @@ class ChannelList extends Component {
             <Icon name="pluscircleo" type="AntDesign" />
           </Button>
         </Header>
-
-        {channelRows}
-      </View>
+        <View>{channelRows}</View>
+      </Container>
     );
   }
 }
