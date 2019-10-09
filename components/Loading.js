1438872e2607fd09459553ec9d4ddf937b563097
AnfalHussain <anfalae95@gmail.com>
2019-10-09 15:49:14 +0300

    ugly native

--- components/Loading.js
+++ components/Loading.js
@@ -8,8 +8,7 @@ class Loading extends Component {
   render() {
     return (
       <Container>
-        {/* <img src={myloading}></img> */}
-        {/* <FontAwesomeIcon icon={faSpinner} spin size="4x" /> */}
+
         <View>
           <Image
             style={{ width: 50, height: 50 }}
